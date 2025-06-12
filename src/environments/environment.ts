a61fb87d208e22d79613e9859d392d336d624d74
Sergio André Gómez Vallejos <102562782+CB-Sergio-AGV@users.noreply.github.com>
2025-06-11 21:31:20 -0500

    Add files via upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
-export const environment = {production: true,
-                            //serverBasePath: "https://my-json-server.typicode.com/estefanojaque/Managewise-reportes"
-                            serverBasePath: 'https://managewise-backend-hfadgugthcc2erdd.centralus-01.azurewebsites.net/api/v1'
-                            //serverBasePath: "http://localhost:3000"
-                            };
+export const environment = {
+  production: true,
+  serverBasePath: 'https://managewise-backend-hfadgugthcc2erdd.centralus-01.azurewebsites.net/api/v1'
+};
